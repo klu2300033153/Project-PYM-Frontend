3417dfbe024aac7a6149e6ebeaa71c2760cc6ca1
klu2300033153 <2300033153@kluniversity.in>
2025-09-21 18:18:23 +0530

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+// import App from './App';
+// import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    {/* <App /> */}
+    <p>Hi, This is PYM</p>
   </BrowserRouter>
 );
